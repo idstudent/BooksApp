c22f8445c1fb565c458c484c8bdfbf2a2e769514
idstudent <36175704+idstudent@users.noreply.github.com>
2022-11-25 11:41:22 +0900

    빠진 커밋 추가

--- app/src/main/java/com/example/booksapp/api/ApiService.kt
+++ app/src/main/java/com/example/booksapp/api/ApiService.kt
@@ -35,7 +35,8 @@ interface ApiService {
         @Query("key") key : String,
         @Query("query") title : String,
         @Query("queryType") queryType : String,
-        @Query("searchTarget") searchType : String,
+        @Query("searchTarget") searchType : String = "book",
+        @Query("start") page : Int = 1,
         @Query("sort") sort : String = "accuracy",
         @Query("soldOut") soldOutType : String = "y",
         @Query("output") output : String = "json"
