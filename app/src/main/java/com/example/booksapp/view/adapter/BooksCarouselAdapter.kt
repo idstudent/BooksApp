a3188fce92bee475b1deeb9d189dd1ba3f855fc7
idstudent <36175704+idstudent@users.noreply.github.com>
2022-11-25 10:54:42 +0900

    뷰홀더 분리

--- app/src/main/java/com/example/booksapp/view/adapter/BooksCarouselAdapter.kt
+++ app/src/main/java/com/example/booksapp/view/adapter/BooksCarouselAdapter.kt
@@ -1,18 +1,14 @@
 package com.example.booksapp.view.adapter
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.booksapp.api.model.BooksModel
 import com.example.booksapp.databinding.ItemViewpagerBookBinding
-import com.example.booksapp.view.BookDetailActivity
-import com.example.booksapp.view.util.setOnSingleClickListener
 
 class BooksCarouselAdapter :
-    ListAdapter<BooksModel.Response.BooksItem, BooksCarouselAdapter.BookViewHolder>(
+    ListAdapter<BooksModel.Response.BooksItem, CarouselBookViewHolder>(
         object : DiffUtil.ItemCallback<BooksModel.Response.BooksItem>() {
             override fun areItemsTheSame(
                 oldItem: BooksModel.Response.BooksItem,
@@ -30,8 +26,8 @@ class BooksCarouselAdapter :
 
         }
     ){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        return BookViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselBookViewHolder {
+        return CarouselBookViewHolder(
             binding = ItemViewpagerBookBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
@@ -40,35 +36,12 @@ class BooksCarouselAdapter :
         )
     }
 
-    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CarouselBookViewHolder, position: Int) {
         holder.onBind(getItem(position))
     }
 
     override fun getItemCount(): Int {
         return if(super.getItemCount() > 6) 6
         else super.getItemCount()
-    }
-
-    class BookViewHolder(
-        private val binding: ItemViewpagerBookBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        init {
-            binding.run {
-                root.setOnSingleClickListener {
-                    item?.let {
-                        val intent = Intent(binding.root.context, BookDetailActivity::class.java)
-                        intent.putExtra("isbn", it.isbn)
-                        if(it.categoryId == "200") {
-                            intent.putExtra("searchType", "foreign")
-                        }
-                        this.root.context.startActivity(intent)
-                    }
-                }
-            }
-        }
-
-        fun onBind(item: BooksModel.Response.BooksItem) {
-            binding.item = item
-        }
     }
 }
