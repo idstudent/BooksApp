10f66b678d446730f601fc300158d8749fc85708
idstudent <36175704+idstudent@users.noreply.github.com>
2023-01-09 09:17:07 +0900

    compose bottom navigation 작업

--- app/src/main/java/com/example/booksapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/booksapp/presentation/view/MainActivity.kt
@@ -1,19 +1,112 @@
 package com.example.booksapp.presentation.view
 
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.example.booksapp.R
-import com.example.booksapp.databinding.ActivityMainBinding
-import dagger.hilt.android.AndroidEntryPoint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.booksapp.*
 
-@AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-    override val layoutId: Int
-        get() = R.layout.activity_main
+class MainActivity : ComponentActivity() {
 
-    override fun initView() {
-        super.initView()
-
-        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreenView()
+        }
     }
+}
+
+@Composable
+fun MainScreenView(){
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigation(navController = navController) },
+    ) {
+
+        NavigationGraph(navController = navController)
+    }
+}
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.BestSeller,
+        BottomNavItem.MyBook,
+        BottomNavItem.BookReport
+    )
+    androidx.compose.material.BottomNavigation(
+        backgroundColor = colorResource(id = R.color.white),
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp
+                    )
+                },
+                selectedContentColor = colorResource(id = R.color.color_f44336),
+                unselectedContentColor = Color.Black,
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.screen_route) {
+        composable(BottomNavItem.Home.screen_route) {
+            HomeScreen()
+        }
+        composable(BottomNavItem.BestSeller.screen_route) {
+            NetworkScreen()
+        }
+        composable(BottomNavItem.MyBook.screen_route) {
+            AddPostScreen()
+        }
+        composable(BottomNavItem.BookReport.screen_route) {
+            NotificationScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationPreview() {
+    MainScreenView()
 }
