e86d87ff1dcfc48769c3d305cfca0e9e2652ed8c
idstudent <36175704+idstudent@users.noreply.github.com>
2023-01-12 09:37:41 +0900

    베스트셀러 버튼눌렀을때 국내, 외국도서 분기 호출 작업 및 ui 작업

--- app/src/main/java/com/example/booksapp/presentation/compose/bottomNavigation/BestSellerScreen.kt
+++ app/src/main/java/com/example/booksapp/presentation/compose/bottomNavigation/BestSellerScreen.kt
@@ -2,19 +2,28 @@ package com.example.booksapp.presentation.compose.bottomNavigation
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.lerp
+import com.bumptech.glide.request.RequestOptions
 import com.example.booksapp.data.api.model.BooksModel
 import com.example.booksapp.presentation.viewmodel.BestSellerViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -27,58 +36,125 @@ import kotlin.math.absoluteValue
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BestSellerScreen(bestSellerViewModel: BestSellerViewModel) {
-    val localBooks = remember { mutableStateListOf<BooksModel.Response.BooksItem>() }
-    val globalBooks = remember { ArrayList<BooksModel.Response.BooksItem>() }
-    var count by remember { mutableStateOf(0) }
+    val books = remember { mutableStateListOf<BooksModel.Response.BooksItem>() }
 
-    LaunchedEffect(key1 = false) {
-        bestSellerViewModel.getBestSellerBookLIst(100).collect {
-            localBooks.addAll(it)
+    val requestOption = rememberSaveable { mutableStateOf(100) }
+
+    var title by rememberSaveable { mutableStateOf("국내 베스트셀러") }
+    var changeBooks by rememberSaveable { mutableStateOf("국내도서 베스트셀러 보기") }
+
+    LaunchedEffect(key1 = requestOption.value) {
+        bestSellerViewModel.getBestSellerBookLIst(requestOption.value).collect {
+            books.clear()
+            books.addAll(it)
         }
     }
 
-    HorizontalPager(
-        count = 5,
-        contentPadding = PaddingValues(horizontal = 32.dp),
+    Column(
         modifier = Modifier.fillMaxSize()
-    ) { page ->
-        Card(
-            Modifier
-                .graphicsLayer {
-                    // Calculate the absolute offset for the current page from the
-                    // scroll position. We use the absolute value which allows us to mirror
-                    // any effects for both directions
-                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp)
+        ) {
+            Text(
+                text = "",
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = title,
+                color = Color.Black,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+            )
 
-                    // We animate the scaleX + scaleY, between 85% and 100%
-                    lerp(
-                        start = 0.85f,
-                        stop = 1f,
-                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                    ).also { scale ->
-                        scaleX = scale
-                        scaleY = scale
+            Text(
+                text = "더보기 >",
+                color = Color.Black,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top = 4.dp, end = 12.dp),
+                textAlign = TextAlign.End,
+            )
+        }
+        HorizontalPager(
+            count = 5,
+            contentPadding = PaddingValues(horizontal = 32.dp),
+            modifier = Modifier.padding(top = 40.dp)
+        ) { page ->
+            if (books.size > 0) {
+                Column {
+                    Card(
+                        Modifier
+                            .graphicsLayer {
+                                val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                                lerp(
+                                    start = 0.85f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                ).also { scale ->
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
+
+                                alpha = lerp(
+                                    start = 0.5f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                )
+                            }
+                    ) {
+                        GlideImage(
+                            imageModel = { books[page].coverLargeUrl },
+                            imageOptions = ImageOptions(
+                                contentScale = ContentScale.Fit
+                            ),
+                            modifier = Modifier
+                                .width(600.dp)
+                                .height(400.dp)
+                        )
                     }
 
-                    // We animate the alpha, between 50% and 100%
-                    alpha = lerp(
-                        start = 0.5f,
-                        stop = 1f,
-                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    Text(
+                        text = books[page].title ?: "",
+                        color = Color.Black,
+                        fontSize = 20.sp,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 12.dp, end = 12.dp, top = 12.dp)
+
                     )
                 }
-        ) {
-            if (localBooks.size > 0) {
-                GlideImage(
-                    imageModel = { localBooks[page].coverLargeUrl },
-                    imageOptions = ImageOptions(
-                        contentScale = ContentScale.FillBounds
-                    ),
-                    modifier = Modifier
-                        .width(600.dp)
-                        .height(400.dp)
-                )
             }
+        }
+        OutlinedButton(
+            onClick = {
+                if (changeBooks == "국내도서 베스트셀러 보기") {
+                    title = "외국 베스트셀러"
+                    changeBooks = "외국도서 베스트셀러 보기"
+                    requestOption.value = 200
+                } else {
+                    title = "국내 베스트셀러"
+                    changeBooks = "국내도서 베스트셀러 보기"
+                    requestOption.value = 100
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 30.dp, bottom = 12.dp)
+        ) {
+            Text(
+                text = changeBooks,
+                fontSize = 20.sp,
+                color = Color.Black,
+            )
         }
     }
 }
